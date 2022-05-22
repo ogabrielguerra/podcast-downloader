4a4295fdc99abed4b1d33dd05df578e2d726c279
Gabriel Guerra <gabrielguerra@gmail.com>
2022-05-22 20:35:19 -0300

    Checking files existence. Minor adjustments. (#1)

--- podcast_downloader.py
+++ podcast_downloader.py
@@ -1,8 +1,9 @@
+from tokenize import String
 from colorama import Back
 import requests
 from bs4 import BeautifulSoup
 from os import mkdir
-from os.path import isdir
+from os.path import isdir, isfile
 
 class PodcastDownloader:
 
@@ -19,13 +20,13 @@ class PodcastDownloader:
         self.populate_set()
                
     
-    def create_target_dir(self):
+    def create_target_dir(self) -> None:
         target_path = self.__TARGET_DIR+'/'+self.__SHOW_TITLE
         if not isdir(self.__TARGET_DIR): mkdir(self.__TARGET_DIR)
         if not isdir(target_path): mkdir(target_path)
 
 
-    def set_source(self, podcast_url, static_source: str=False):
+    def set_source(self, podcast_url, static_source: str=False) -> None:
         if static_source:
             source = open(static_source, 'r')
             html_source = source.read()
@@ -37,7 +38,7 @@ class PodcastDownloader:
         self.__html_source = html_source    
 
 
-    def populate_set(self):
+    def populate_set(self) -> None:
         parsed_html = BeautifulSoup(self.__html_source, 'html.parser')
 
         for link in parsed_html.findAll('a'):
@@ -45,21 +46,26 @@ class PodcastDownloader:
             if href and r".mp3" in href: self.__files_path.add(href)
 
 
-    def get_name(self, name_string):
+    def get_name(self, name_string) -> String:
         name_string = name_string.split('/')
         name_string = name_string[len(name_string)-1]
         name_string = name_string.split('?')
         return name_string[0]
 
 
-    def download(self):
+    def download(self) -> None:
 
         print(Back.BLUE + f'\n\nPODCAST DOWNLOADER\nTotal files: {len(self.__files_path)}\n')
 
         for url in self.__files_path:
             
             name = self.get_name(url)
+            file = self.__TARGET_DIR + '/' + self.__SHOW_TITLE + '/' + name
 
-            response = requests.get(url, allow_redirects=True)
-            open(self.__TARGET_DIR + '/' + self.__SHOW_TITLE + '/' + name, 'wb').write(response.content)
-            print(Back.GREEN + f"File {url}")
+            if not isfile(file):
+                response = requests.get(url, allow_redirects=True)
+                open(file, 'wb').write(response.content)
+                print(Back.GREEN + f"+ Downloaded [{name}].")
+            else:
+                print(Back.RED + f"- Skiping [{name}] because it already exists.")
+                
